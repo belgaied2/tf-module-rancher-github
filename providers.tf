76f6c1f77b9d2feea8c5306dbb54f4cfc8afffb7
Mohamed Belgaied Hassine <60746935+belgaied2@users.noreply.github.com>
2021-05-27 13:22:39 +0200

    Update providers.tf
    Updated providers with the right registry path: rancher/rancher2

--- providers.tf
+++ providers.tf
@@ -4,6 +4,15 @@ provider "rancher2" {
     api_url = var.api_url
 }
 
+terraform {
+  required_providers {
+    rancher2 = {
+      source = "rancher/rancher2"
+      version = "1.15.1"
+    }
+  }
+}
+
 provider "github" {
     token = var.github_token
     organization = var.organization
